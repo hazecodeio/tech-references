34bbc736571f5b8163c02a25a235680ed81320f1
hsmak <husain.alkhamis@gmail.com>
2022-09-04 13:34:26 -0500

    committing changes...

--- scripts-of-bash/network-restart.sh
+++ scripts-of-bash/network-restart.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 sudo bash -c '
-echo "Restarting Network in 20 Sec";
-ip link set wlp34s0 down;
-sleep 20;
-ip link set wlp34s0 up
-echo "Running SpeedTest in 10 Sec";
+sudo nmcli networking off;
+sudo nmcli  networking on;
+echo "Speedtesing after 10 secs...";
 sleep 10;
 speedtest;
 '

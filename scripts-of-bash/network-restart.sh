9dac7bde6e8f5536e69b3849777a4f66d9551bbe
hsmak <husain.alkhamis@gmail.com>
2023-01-16 18:19:39 -0600

    committing changes...

--- scripts-of-bash/network-restart.sh
+++ scripts-of-bash/network-restart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo bash -c '
-sudo nmcli networking off;
-sudo nmcli  networking on;
+bash -c '
+nmcli networking off;
+nmcli  networking on;
 echo "Speedtesing after 10 secs...";
 sleep 10;
 speedtest;

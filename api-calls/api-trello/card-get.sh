b2d0ae80a1d3e4b5e11fc151476d119658d5dd6e
hazecodeio <haze.codeio@gmail.com>
2024-02-27 10:38:49 -0600

    committing changes...

--- api-calls/api-trello/card-get.sh
+++ api-calls/api-trello/card-get.sh
@@ -3,7 +3,5 @@ source ./_env-loader.sh
 
 curl -H "Authorization: OAuth oauth_consumer_key=\"$KEY_TRELLO\", oauth_token=\"$TOKEN_TRELLO\"" \
   -H 'Accept: application/json' \
-	--request PUT \
-	--url 'https://api.trello.com/1/cards/ugJwkxmU' \
-	-d '{"cover": {"color":"yellow","idAttachment":null,"idUploadedBackground":null,"size":"normal","brightness":"light"}}'
-
+	--request GET \
+	--url 'https://api.trello.com/1/cards/ugJwkxmU'

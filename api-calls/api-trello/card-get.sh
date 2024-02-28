f442762c45cca8d9ac2a915eef26a7263960dd5c
hazecodeio <haze.codeio@gmail.com>
2024-02-27 19:15:11 -0600

    committing changes...

--- api-calls/api-trello/card-get.sh
+++ api-calls/api-trello/card-get.sh
@@ -1,7 +1,10 @@
 # https://developer.atlassian.com/cloud/trello/guides/rest-api/authorization/#passing-token-and-key-in-api-requests
 source ./_env-loader.sh
 
+CARD_ID=ugJwkxmU
+CARD_ID=UAHsXfjc
+
 curl -H "Authorization: OAuth oauth_consumer_key=\"$KEY_TRELLO\", oauth_token=\"$TOKEN_TRELLO\"" \
   -H 'Accept: application/json' \
 	--request GET \
-	--url 'https://api.trello.com/1/cards/ugJwkxmU'
+	--url "https://api.trello.com/1/cards/$CARD_ID" |jq
--- api-calls/api-trello/card-put.sh
+++ api-calls/api-trello/card-put.sh
@@ -1,9 +1,11 @@
 # https://developer.atlassian.com/cloud/trello/guides/rest-api/authorization/#passing-token-and-key-in-api-requests
 source ./_env-loader.sh
 
+CARD_ID=zIMl69o3
+
 curl -H "Authorization: OAuth oauth_consumer_key=\"$KEY_TRELLO\", oauth_token=\"$TOKEN_TRELLO\"" \
   -H 'Accept: application/json' \
 	--request PUT \
-	--url 'https://api.trello.com/1/cards/oV6vk7Do' \
-	-d '{"cover": {"color":"yellow","idAttachment":null,"idUploadedBackground":null,"size":"normal","brightness":"light"}}'
+	--url "https://api.trello.com/1/cards/$CARD_ID" \
+	-d '{"cover": {"color":"yellow","idAttachment":65de059d6c0cda16e85f9c1d,"idUploadedBackground":null,"size":"normal","brightness":"light"}}'
 

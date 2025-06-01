f1b7ebcee83e33ab8033822fd1d38565751bc524
hazecodeio <3461124+hazecodeio@users.noreply.github.com>
2025-06-01 00:08:45 -0500

    committing changes...

--- os-tools/resources-brave/fun-jsonpaths.sh
+++ os-tools/resources-brave/fun-jsonpaths.sh
@@ -5,8 +5,8 @@ EXT_ID=ghmbeldphafepmbegfdlkpapadhbakde
 # NOTE: You still need to slurp the json objects for GroupBy to work.
 fun_jsonpaths_key_to_value_array() {
 
-  find ${BASEDIR} \
-      \( -iregex '.*Profile\@.*vid.*' -iregex '.*Preferences.*'  \) -type f  \
+  find ${BASEDIR} -regextype posix-extended  \
+      \( -iregex '.*Profile\@.*' -iregex '.*/Preferences$'  \) -type f  \
       \( -exec grep -iEl '.*ghmbel.*' {} \;  \)  \
   | xargs -i jq --arg P {} '. + {filepath: $P}' {} \
   | jq 'include "m"; {  filepath:   .filepath,
@@ -90,23 +90,42 @@ fun_jsonpaths_key_dotted_filtered() {
 
 
 
+
+
+
+
+
 # ToDo - automate with: cd, cp, mkdir, etc... Automate Backing up files being modified
 fun_jsonpaths_delete_leaves() {
-  OUT=$(fun_jsonpaths_key_array_filtered | jq -r '.[0].jsonpaths | tojson')
 
-  cat ${HOME}/.config/BraveSoftware/brave-browser-*/Profile@*Vid*/Preferences \
+  PROFILE=.*Profile@.*Queerly.*
+
+  OUT=$(fun_jsonpaths_key_array_filtered | jq -r --arg PROF ${PROFILE} '.[] | select(.filepath | test($PROF; "ig") and endswith("Preferences")) | .jsonpaths | tojson')
+
+  find ${BASEDIR} -regextype posix-extended  \
+        \( -iregex ${PROFILE} -iregex '.*/Preferences$'  \) -type f  \
+  | xargs -i cat {} \
   | jq --argjson P ${OUT} 'delpaths($P)' \
-#  | jq --arg kw ${EXT_ID} '.extensions.settings | {ghmbeldphafepmbegfdlkpapadhbakde}' | grep -i '\|ghmbeldphafepmbegfdlkpapadhbakde'
+  | jq --arg kw ${EXT_ID} '.extensions.settings | {ghmbeldphafepmbegfdlkpapadhbakde}' | grep -i '\|ghmbeldphafepmbegfdlkpapadhbakde'
 }
 
 
 fun_jsonpaths_delete_parent() {
-  OUT=$(fun_jsonpaths_key_array_filtered | jq --arg kw ${EXT_ID} -r '.[0].jsonpaths | map(.[:index($kw)+1]) | tojson')
 
-  cat ${HOME}/.config/BraveSoftware/brave-browser-*/Profile@*Vid*/Preferences \
+  PROFILE=.*Profile@.*Queerly.*
+
+  OUT=$(fun_jsonpaths_key_array_filtered | jq -r --arg PROF ${PROFILE} --arg kw ${EXT_ID} '.[] | select(.filepath | test($PROF; "ig") and endswith("Preferences")) | .jsonpaths | map(.[:index($kw)+1]) | tojson')
+
+  find ${BASEDIR} -regextype posix-extended  \
+          \( -iregex ${PROFILE} -iregex '.*/Preferences$'  \) -type f  \
+  | xargs -i cat {} \
   | jq --argjson P ${OUT} 'delpaths($P)' \
-#  | jq --arg kw ${EXT_ID} '.extensions.settings | {ghmbeldphafepmbegfdlkpapadhbakde}' | grep -i '\|ghmbeldphafepmbegfdlkpapadhbakde'
+  | jq --arg kw ${EXT_ID} '.extensions.settings | {ghmbeldphafepmbegfdlkpapadhbakde}' | grep -i '\|ghmbeldphafepmbegfdlkpapadhbakde'
 }
+
+
+
+
 
 
 

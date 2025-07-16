0acd3c165bcb821e3780d8b5b86e8e39ea3d28b5
hazecodeio <3461124+hazecodeio@users.noreply.github.com>
2025-07-15 19:40:12 -0500

    committing changes...

--- utils/data-mining/jq-utils.sh
+++ utils/data-mining/jq-utils.sh
@@ -88,3 +88,35 @@ fun_unzip_to_Dir() {
   | xargs -i jq -n --arg p {} '{p:$p,s:$p|rtrimstr(".zip")} | (.p, .s)' \
   | xargs -n2 bash -c 'unzip -o "$0" -d "$1"'
 }
+
+fun_find_files_not_matching_fltr() {
+  FLTR=$(jq -rn '["pdf", "image", "audio", "mpeg", "iso"] | tojson')
+
+  find $(pwd)/ -not -iregex '.*jpg' -type f -exec file {} + \
+  | jq  -R --argjson fltr $FLTR 'split(":")  | map(.|sub("^[ ]+|[ ]+$"; ""))
+                        | { path: .[0],
+                            type: .[1] | select( [ascii_downcase] | map( contains($fltr[]) ) | any | not)
+#                                      | select( ascii_downcase | contains($fltr[]) | not) # might not work for the negation
+                          } '
+}
+
+
+fun_brave_crx_groupby() {
+  find  ${HOME}/.config/BraveSoftware/brave-browser-*/*crx*/metadata.json -not -iregex .*tpac.* \
+  | xargs -i jq --arg p {} '{ p: $p,
+                              groupby: $p | split("/")[-2]
+                            } + .' {} \
+  | jq -s 'group_by(.groupby) | to_entries | map({ groupby: .value[0].groupby, value: .value | map(del(.groupby)) })'
+}
+
+fun_merge_dirs() {
+  SRC=${HOME}/Documents/temp
+  DST=${HOME}/Documents/temp2
+
+  # create subdirectories with mkdir -p
+  find "${SRC}" -type f | jq -R 'split("/")[8:-1]|join("/")' | jq -s 'unique[]' | xargs -i mkdir -p "${DST}"/{}
+
+  find "${SRC}" -type f \
+  | jq -Rr --arg dst ${DST} '{src: ., dst: [$dst, (. | split("/")[8:]|join("/")) ] | join("/") } | (.src, .dst) | tojson' \
+#  | xargs -n2 bash -c 'mv "$0" "$1" '
+}
